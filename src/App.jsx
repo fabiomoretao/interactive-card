b4aa102954fb584bcc38f82ee13bffd4ffa3dbf3
Fabio Moretão <fabiojosemoretaodesouza@gmail.com>
2025-07-29 22:26:42 -0300

    feat(ui): tornar layout responsivo até 1920×1080

--- src/App.jsx
+++ src/App.jsx
@@ -12,46 +12,55 @@ export default function App() {
   const [month, setMonth] = useState('')
   const [year, setYear] = useState('')
   const [isCompleted, setIsCompleted] = useState(false)
-  
-  function handleCompleted () {
+
+  function handleCompleted() {
     setIsCompleted(prev => !prev)
   }
 
   return (
-    <div className="flex flex-col">
-      <img
-        src={bgImg}
-        className="z-0 max-h-[20%]"
-      />
-
-      <Cards
-        cardholder={cardholder}
-        cardNumber={cardNumber}
-        cvc={cvc}
-        month={month}
-        year={year}
-      />
-
-      {isCompleted ?
-        <div className="flex flex-col justify-content-center items-center mt-[2rem]">
-          <img src={iconComplete} alt="You complete! S2" width={"25%"}/>
-          <h1 className="mt-[1rem] mb-[.5rem] text-3xl tracking-widest">Thank You!</h1>
-          <p className="mb-[1rem] text-base normal-case text-gray-400">We`ve added your card details</p>
-        </div> :
-        <Form
-          cardholder={cardholder}
-          setCardholder={setCardholder}
-          cardNumber={cardNumber}
-          setCardNumber={setCardNumber}
-          cvc={cvc}
-          setCvc={setCvc}
-          month={month}
-          setMonth={setMonth}
-          year={year}
-          setYear={setYear}
+    <div className="flex flex-col max-h-screen h-screen 
+    laptop:grid laptop:grid-cols-[minmax(0,40%)_1fr]">
+      <div className="relative
+      laptop:col-start-1 laptop:flex laptop:flex-row laptop:max-h-screen">
+        <img
+          src={bgImg}
+          className="z-0 max-h-[20rem] min-w-full
+          laptop:h-full laptop:max-h-none laptop:object-cover"
         />
-      }
-      <button onClick={handleCompleted}>Confirm</button>
+
+        <Cards
+          cardholder={cardholder}
+          cardNumber={cardNumber}
+          cvc={cvc}
+          month={month}
+          year={year}
+        />
+      </div>
+      <div className="laptop:col-start-2 laptop:flex laptop:flex-col laptop:justify-center laptop:ml-[20%] laptop:mr-[10%]">
+        {isCompleted ?
+          <div className="flex flex-col justify-content-center items-center mt-[15%] mb-[3rem] ">
+            <img src={iconComplete} alt="You complete! S2" width={"25%"} />
+            <h1 className="mt-[1rem] mb-[.5rem] text-3xl tracking-widest">Thank You!</h1>
+            <p className="mb-[1rem] text-base normal-case text-gray-400">We`ve added your card details</p>
+          </div> :
+          <Form
+            cardholder={cardholder}
+            setCardholder={setCardholder}
+            cardNumber={cardNumber}
+            setCardNumber={setCardNumber}
+            cvc={cvc}
+            setCvc={setCvc}
+            month={month}
+            setMonth={setMonth}
+            year={year}
+            setYear={setYear}
+            className="laptop:col-[2]"
+          />
+        }
+        <button
+          onClick={handleCompleted}
+        >Confirm</button>
+      </div>
     </div>
   )
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,49 +3,67 @@ import cardFront from "../assets/bg-card-front.png"
 import cardLogo from "../assets/card-logo.svg"
 
 function formatCardNumber(number) {
-  const digitsOnly = number.replace(/\D/g, '');
-  const groups = digitsOnly.match(/.{1,4}/g) || [];
-  return groups.join(' ');
+    const digitsOnly = number.replace(/\D/g, '');
+    const groups = digitsOnly.match(/.{1,4}/g) || [];
+    return groups.join(' ');
 }
 
 export default function Cards(props) {
     return (
-        <div className="w-full max-w-7/10 -mt-[50%] self-center">
-            <div className="w-full max-w-sm ml-[10%] z-10 flex items-center">
+        <div className="w-[80%] absolute transform -translate-y-[80%] flex flex-col items-center mx-4
+        laptop:translate-y-[50%] laptop:translate-x-[50%]">
+            <div className="w-full max-w-sm ml-[30%] z-10 flex items-center shadow-[0_0_10px_1px_rgba(0,0,0,0.8)] rounded-lg
+                tablet:transform tablet:-translate-y-[10%]
+                laptop:translate-y-full">
                 <img
                     src={cardBack}
                     alt="back of credit card :o"
                     className="block w-full h-auto" />
                 <span
-                    className="text-white -translate-x-full pr-[10%] text-sm"
+                    className="text-white transform -translate-x-full pr-[15%] text-sm
+                    tablet:text-xl
+                    laptop:text-sm laptop:translate-x-[-25%] laptop:mx-[-15%]
+                    laptop-lg:text-xl"
                 >
                     {props.cvc ? props.cvc : "000"}
                 </span>
             </div>
-            <div className="w-full max-w-sm flex -ml-[10%] -translate-y-[25%] -my-[10%] z-20 ">
+            <div className="w-full max-w-sm flex transform -translate-y-[25%] -my-[10%] z-20 shadow-[0_0_10px_1px_rgba(0,0,0,0.8)] rounded-lg
+            laptop:transform laptop:-translate-y-full laptop:-translate-x-[10%]
+            laptop-lg:text-lg">
                 <img
                     src={cardLogo}
-                    alt="front of credit card :p"
+                    alt="logo of credit card :p"
                     className="absolute z-30 w-[20%] h-auto ml-[5%] mt-[5%]" />
                 <img
                     src={cardFront}
-                    alt="back of credit card :o"
+                    alt="front of credit card :o"
                     className="block w-full h-auto" />
 
                 <div className="grid grid-cols-[70% 30%] w-[100%] -translate-x-[50%] -mx-[50%] mt-[25%] items-top px-[1rem]">
                     <span
-                        className="text-white tracking-widest col-[1/3] justify-self-center"
+                        className="text-white tracking-widest col-[1/3] justify-self-center
+                        mobile:text-[22px] 
+                        tablet:text-[25px]
+                        laptop:text-[20px]
+                        laptop-lg:text-[28px]"
                         style={{ maxWidth: "100%", overflow: "hidden", whiteSpace: "nowrap" }}
                     >
                         {props.cardNumber ? formatCardNumber(props.cardNumber) : "1234 5678 9123 0000"}
                     </span>
                     <span
-                        className="text-white text-sm col-[1] truncate mr-[1rem] min-w-[90%]"
+                        className="text-white text-sm col-[1] truncate mr-[1rem] min-w-[90%]
+                        tablet:text-xl
+                        laptop:text-sm
+                        laptop-lg:text-lg"
                     >
                         {props.cardholder ? props.cardholder : "Jane Appleseed"}
                     </span>
                     <span
-                        className="text-white text-sm whitespace-nowrap col-[2] justify-self-end"
+                        className="text-white text-sm whitespace-nowrap col-[2] justify-self-end
+                        tablet:text-xl
+                        laptop:text-sm
+                        laptop-lg:text-lg"
                     >
                         {props.month ? props.month : "00"}/{props.year ? props.year : "00"}
                     </span>
